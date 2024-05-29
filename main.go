be3be7695df3d4e03c91e15128cbc5bd4640fc2e
icaro <darkflamemaster663@gmail.com>
2024-05-29 00:31:05 -0300

    move orderbook file and create webserver

--- main.go
+++ main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/KurobaneShin/crypto-exchange/orderbook"
+	echo "github.com/labstack/echo/v4"
+)
 
 func main() {
+	e := echo.New()
+
+	e.Start(":3000")
+
 	fmt.Println("working")
+}
+
+type Market string
+
+const(
+	MarketETH Market = "ETH"
+)
+
+type Exchange struct {
+	orderbooks map[Market]*orderbook.Orderbook
+}
+
+func NewExchange()*Exchange{
+	return &Exchange{
+		orderbooks:make(map[Market]*orderbook.Orderbook),
+	}
+}
+
+func handlePlaceOrder(c echo.Context) error {
+	return c.JSON(200,"SSS")
 }
